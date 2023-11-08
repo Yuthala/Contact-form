ae33edd1e6c41b7de26fe7529b91ec915c4ca232
Dina Polousova <dinapolousova@gmail.com>
2023-11-08 11:10:23 +0300

    html

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Contact Form</title>
+</head>
+<body>
+	<form action="" method="POST" id="contect">
+		<p>
+			<label for="name">Имя</label>
+			<input type="text" name="name" id="name"><span></span>
+		</p>
+		<p>
+			<label for="subject">Тема</label>
+			<input type="text" name="subject" id="subject"><span></span>
+		</p>
+		<p>
+			<label for="email">Email</label>
+			<input type="text" name="email" id="email"><span></span>
+		</p>
+		<p>
+			<label for="body">Сообщение</label>
+			<textarea name="body" cols="30" rows="10" id="body"></textarea><span></span>
+		</p>
+		<p>
+			<input id="submit" type="submit" name="submit" value="Отправить"><span></span>
+		</p>
+	</form>
+</body>
+</html>
