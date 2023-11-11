bc7e55fd85266cf1de4b9edeb4fe22c7766b1a36
Dina Polousova <dinapolousova@gmail.com>
2023-11-11 10:44:10 +0300

    syntax error json

--- script.js
+++ script.js
@@ -7,30 +7,33 @@ async function postData(url= '', data = {}) {
         body: data
     });
     return await  response.json();
+
 }
 
 // отправка
 let form = document.getElementById('contact'); // переменная с формой
 // при отправке формы любым способом
+console.log(`Значение form ${form}`);
 form.addEventListener('submit', function (event) {
     // запрещаем стандартное действие
     event.preventDefault();
     // создаем объект новый
-    let data = new FormData(form);
+    const data = new FormData(form);
 	console.log(`Значение переменной ${data}`);
     // передаем в фукцию fetch данные и получаем результат
-    postData('send.php', data).then((data) => {
-        // обработка ответа от сервера
-        console.log(data);
-        if (data.error == '') {
-            alert(data.success);
-            cleanForm();
-        } else if (data.email !== '') {
-            alert(data.email);
-        } else {
-            alert(data.error);
-        }
-    })
+    // postData('send.php', data).then((data) => {
+    //     // обработка ответа от сервера
+    //     console.log(data);
+    //     if (data.error == '') {
+    //         alert(data.success);
+    //         cleanForm();
+    //     } else if (data.email !== '') {
+    //         alert(data.email);
+    //     } else {
+    //         alert(data.error);
+    //     }
+    // })
+	postData('send.php', data);
 })
 
 });
